eba8da7ff831dcc863a0d282cc10248b67a00e16
TemplaRus <RusTemplaR@yandex.com>
2015-11-13 18:24:23 +0300

    first try на скриншоте - цель = распознать полоску WASD

--- src/bdriver/BDriver.java
+++ src/bdriver/BDriver.java
@@ -1,8 +1,19 @@
 package bdriver;
 
+import java.awt.AWTException;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.sourceforge.javaocr.awt.*;
-
+import net.sourceforge.javaocr.ocr.PixelImage;
+import net.sourceforge.javaocr.ocrPlugins.mseOCR.CharacterRange;
+import net.sourceforge.javaocr.ocrPlugins.mseOCR.OCRScanner;
 
 public class BDriver {
 
@@ -10,7 +21,35 @@ public class BDriver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        GraphicsDevice gd;
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice[] gs = ge.getScreenDevices();
+        gd = gs[0];
+        //        for (int j = 0; j < gs.length; j++) {
+//             gd= gs[j];
+//            GraphicsConfiguration[] gc = gd.getConfigurations();
+//        }
+
+        Robot r;
+        try {
+            r = new Robot(gd);
+
+            Rectangle rect = new Rectangle(780, 400, 375, 25);
+            BufferedImage im = r.createScreenCapture(rect);
+            Character c = 'a';//new Character('a');
+            CharacterRange[] cr = new CharacterRange[3];
+
+            cr[0] = new CharacterRange(65);
+            cr[1] = new CharacterRange(87);
+            cr[2] = new CharacterRange(83);
+            cr[3] = new CharacterRange(68);
+
+            OCRScanner ocr = new OCRScanner();
+            System.out.println("Scan=" + ocr.scan(im, 100, 100, 200, 200, cr));
+
+        } catch (AWTException ex) {
+            Logger.getLogger(BDriver.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
