8899bbe946292521ef56c4d35bd6dab767f01cd8
TemplaRus <RusTemplaR@yandex.com>
2015-11-13 17:04:40 +0300

    First steps

---
+++ src/bdriver/BDriver.java
@@ -0,0 +1,16 @@
+package bdriver;
+
+import java.awt.Robot;
+import net.sourceforge.javaocr.awt.*;
+
+
+public class BDriver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
