d55a179324c826e209c9c1cc24022198383de07e
TemplaRus <RusTemplaR@yandex.com>
2015-11-14 07:34:16 +0300

    попытка скомуниздить медианный фильтр - а потом ещё и монохромный делать..

--- src/bdriver/BDriver.java
+++ src/bdriver/BDriver.java
@@ -2,15 +2,25 @@ package bdriver;
 
 import java.awt.AWTException;
 import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.Robot;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
+import java.awt.image.FilteredImageSource;
+import java.awt.image.ImageFilter;
+import java.awt.image.ImageProducer;
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.GrayFilter;
+import net.sourceforge.javaocr.ocrPlugins.mseOCR.CharacterRange;
+import net.sourceforge.javaocr.ocrPlugins.mseOCR.OCRScanner;
 
 public class BDriver {
 
@@ -26,54 +36,48 @@ public class BDriver {
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice[] gs = ge.getScreenDevices();
         gd = gs[0];
-        //        for (int j = 0; j < gs.length; j++) {
-//             gd= gs[j];
-//            GraphicsConfiguration[] gc = gd.getConfigurations();
-//        }
-
         Robot r;
-        try
-        {
+        for (int j = 0; j < gs.length; j++) {
+            gd = gs[j];
+            GraphicsConfiguration[] gc = gd.getConfigurations();
+        }
+
+        try {
             r = new Robot(gd);
 
             Rectangle rect = new Rectangle(780, 400, 375, 25);
             BufferedImage im = r.createScreenCapture(rect);
             File f = new File("D:\\JAVA\\javaprojects\\BDriver\\buttonsfullscreen1.JPG");
-//            File f2 = new File("D:\\JAVA\\javaprojects\\BDriver\\buttonsfullscreen1GS.JPG");
-//            File f3 = new File("D:\\JAVA\\javaprojects\\BDriver\\test.JPG");
-//
-//            CharacterRange[] cr = new CharacterRange[4];
-//
-//            cr[0] = new CharacterRange(65);
-//            cr[1] = new CharacterRange(87);
-//            cr[2] = new CharacterRange(83);
-//            cr[3] = new CharacterRange(68);
-//
-//            OCRScanner ocr = new OCRScanner();
-//            System.out.println("file data heigh=" + ImageIO.read(f).getHeight() + "  width=" + ImageIO.read(f).getWidth());
-//            Image image = ImageIO.read(f);
-//            ImageFilter filter = new GrayFilter(true, 25);
-//            ImageProducer producer = new FilteredImageSource(image.getSource(), filter);
-//            Image grayimage = Toolkit.getDefaultToolkit().createImage(producer);
-//
-//            ImageIO.write(getRenderedImage(grayimage), "PNG", f2);
-//            if (f.canRead())
-//            {
-//
-//                System.out.println("Scan=" + ocr.scan(ImageIO.read(f), 780, 400, 375, 25, cr));
-//                //    System.out.println("Scan=" + ocr.scan(ImageIO.read(f3), 10, 10, 375, 325, cr));
-//                System.out.println("Scan=" + ocr.scan(getRenderedImage(grayimage), 780, 400, 375, 25, cr));
-//
-//            }
-//            // System.out.println("Scan=" + ocr.scan(im, 0, 0, 375, 25, cr));
-            //String s = ocr2.recognize("D:\\JAVA\\javaprojects\\BDriver\\buttonsfullscreen1.JPG", -1, 780, 400, 375, 25, Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PLAINTEXT);
-            
+            File f2 = new File("D:\\JAVA\\javaprojects\\BDriver\\buttonsfullscreen1GS.JPG");
+            File f3 = new File("D:\\JAVA\\javaprojects\\BDriver\\test.JPG");
 
-        } catch (AWTException ex)
-        {
+            CharacterRange[] cr = new CharacterRange[4];
+
+            cr[0] = new CharacterRange(65);
+            cr[1] = new CharacterRange(87);
+            cr[2] = new CharacterRange(83);
+            cr[3] = new CharacterRange(68);
+
+            OCRScanner ocr = new OCRScanner();
+            System.out.println("file data heigh=" + ImageIO.read(f).getHeight() + "  width=" + ImageIO.read(f).getWidth());
+            Image image = ImageIO.read(f);
+            ImageFilter filter = new GrayFilter(true, 25);
+            ImageProducer producer = new FilteredImageSource(image.getSource(), filter);
+            Image grayimage = Toolkit.getDefaultToolkit().createImage(producer);
+
+            ImageIO.write(getRenderedImage(grayimage), "PNG", f2);
+            if (f.canRead()) {
+
+                System.out.println("Scan=" + ocr.scan(ImageIO.read(f), 780, 400, 375, 25, cr));
+                //    System.out.println("Scan=" + ocr.scan(ImageIO.read(f3), 10, 10, 375, 325, cr));
+                System.out.println("Scan=" + ocr.scan(getRenderedImage(grayimage), 780, 400, 375, 25, cr));
+
+            }
+            // System.out.println("Scan=" + ocr.scan(im, 0, 0, 375, 25, cr));
+
+        } catch (AWTException ex) {
             Logger.getLogger(BDriver.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (IOException ex)
-//        {
+        } catch (IOException ex) {
             Logger.getLogger(BDriver.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -89,4 +93,6 @@ public class BDriver {
         g2.dispose();
         return out;
     }
+
+
 }
